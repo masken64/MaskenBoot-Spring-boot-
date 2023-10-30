73a296395b105902486a0cf659c8024f353982ae
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-10-30 14:08:47 +0530

    initial commit

---
+++ SimpleAuthentication/src/main/java/com/masken/SimpleAuthentication/SimpleAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.masken.SimpleAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuthenticationApplication.class, args);
+	}
+
+}
---
+++ SimpleAuthentication/src/test/java/com/masken/SimpleAuthentication/SimpleAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masken.SimpleAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
