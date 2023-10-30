abe03c44f8e259e5a493704cc33d944203b8f48d
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-10-30 14:35:57 +0530

    Simple Login

--- SimpleAuthentication/src/main/java/com/masken/SimpleAuthentication/SimpleAuthenticationApplication.java
+++ SimpleAuthentication/src/main/java/com/masken/SimpleAuthentication/SimpleAuthenticationApplication.java
@@ -2,8 +2,10 @@ package com.masken.SimpleAuthentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class SimpleAuthenticationApplication {
 
 	public static void main(String[] args) {
